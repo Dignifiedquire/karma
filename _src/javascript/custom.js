f081b30623f40cee43de2d459ef9a873d3285f27
Friedel Ziegelmayer <friedel.ziegelmayer@gmail.com>
2013-02-13 22:21:25 +0100

    make youtube videos original size

--- _src/javascript/custom.js
+++ _src/javascript/custom.js
@@ -43,36 +43,6 @@
       directionNav: false,
       controlsContainer: '.flex-container'
     });
-
-    $('.flexslider2').flexslider({
-      animation: 'slide',
-      animationLoop: true,             //Boolean: Should the animation loop? If false, directionNav will received "disable" classes at either end
-      slideshow: true,                //Boolean: Animate slider automatically
-      slideshowSpeed: 4500,           //Integer: Set the speed of the slideshow cycling, in milliseconds
-      animationSpeed: 700,             //Boolean: Pause the slideshow when interacting with control elements, highly recommended.
-      pauseOnHover: true,
-      pauseOnAction:false,
-      controlNav: false,
-      directionNav: true,
-      controlsContainer: '.flex-container'
-    });
-
-    $('.flexslider3').flexslider({
-      animation: 'slide',
-      animationLoop: true,             //Boolean: Should the animation loop? If false, directionNav will received "disable" classes at either end
-      slideshow: false,                //Boolean: Animate slider automatically
-      slideshowSpeed: 4500,           //Integer: Set the speed of the slideshow cycling, in milliseconds
-      animationSpeed: 700,             //Boolean: Pause the slideshow when interacting with control elements, highly recommended.
-      pauseOnHover: true,
-      pauseOnAction:false,
-      controlNav: false,
-      directionNav: true,
-      controlsContainer: '.flex-container'
-    });
-
-
   });
 
 })(jQuery);
-
-
